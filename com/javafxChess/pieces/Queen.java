eeb4b5788b32dbfbccb98ed7316291d961baa5ed
Matthew Jones <awsumatt@gmail.com>
2019-04-06 15:52:11 -0400

    Added basic costructor for Queen

--- com/javafxChess/pieces/Queen.java
+++ com/javafxChess/pieces/Queen.java
@@ -1,1 +1,18 @@
 package com.javafxChess.pieces;
+
+
+public class Queen extends Piece{
+
+	public Queen(boolean team, int[] location){
+		super(team, location);
+	}
+
+	/**
+	*@return True if move valid, false otherwise
+	*/
+	public boolean move(int[][] board){
+
+		 return false;
+	}
+
+}
