c638f3286d00a7819adc4c22d35544e8ffa7445d
Matthew Jones <awsumatt@gmail.com>
2019-04-06 23:46:07 -0400

    Rook constructor format fix

--- com/javafxChess/pieces/Rook.java
+++ com/javafxChess/pieces/Rook.java
@@ -2,14 +2,14 @@ package com.javafxChess.pieces;
 
 public class Rook extends Piece{
 	private Boolean canCastle;
-	private String side;
-	Rook(int[][] loc, String side) {
-		super(loc);
+
+	Rook(boolean team, int[] location) {
+		super(team, location);
 		canCastle = true;
-		this.side = side;
 	}
-	
-	public Boolean move(int[] loc) {
+
+	@Override
+	public boolean move(int[] loc) {
 		if(super.getX() == loc[0] && super.getY() != loc[1]) {;
 			super.location[1] = loc[1];
 			canCastle = false;
@@ -21,11 +21,11 @@ public class Rook extends Piece{
 		}
 		return false;
 	}
-	
+
 	public Boolean getCanCastle() {
 		return canCastle;
 	}
-	
+
 	public String getSide() {
 		return side;
 	}
