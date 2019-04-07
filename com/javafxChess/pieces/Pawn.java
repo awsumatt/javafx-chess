f8fea5431bbc2eda1fd153d615c824739a093bb3
Matthew Jones <awsumatt@gmail.com>
2019-04-06 23:57:11 -0400

    Added pawn constructor for work on board

--- com/javafxChess/pieces/Pawn.java
+++ com/javafxChess/pieces/Pawn.java
@@ -2,4 +2,16 @@ package com.javafxChess.pieces;
 
 public class Pawn extends Piece{
 
+
+	/**
+	*Initializes pawn with a specific location
+	*
+	*@param team true=white team, false=black team
+	*@param location Location in (x,y) format
+	*/
+	public Pawn(boolean team, int[] location){
+		super(team, location);
+	}
+
+
 }
