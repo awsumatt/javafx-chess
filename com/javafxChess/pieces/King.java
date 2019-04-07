7bb05606276f29f7ab27023e4d9aca965d8c8ec5
Matthew Jones <awsumatt@gmail.com>
2019-04-06 23:54:28 -0400

    Added King constructors to make it easier to work on board

--- com/javafxChess/pieces/King.java
+++ com/javafxChess/pieces/King.java
@@ -3,15 +3,38 @@ package com.javafxChess.pieces;
 
 public class King extends Piece{
 
+	/**
+	*Initializes king with a specific location
+	*
+	*@param team true=white team, false=black team
+	*@param location Location in (x,y) format
+	*/
 	public King(boolean team, int[] location){
 		super(team, location);
+	}
+
+	/**
+	*Initializes king with the default location
+	*
+	*@param team true=white team, false=black team
+	*/
+	public King(boolean team){
+		super(team);
+		if(team){ //White team default location
+			location[0] = 3;
+			location[1] = 7;
+		} else { //Black team default location
+			location[0] = 3;
+			location[1] = 0;
+		}
+
 	}
 
 	/**
 	*@return True if move valid, false otherwise
 	*/
 	public boolean move(int[][] board){
-		
+
 		 return false;
 	}
 
