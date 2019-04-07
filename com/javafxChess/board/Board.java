ccf2e6aef4d794aad6c1931df1bec9214d3a838b
Matthew Jones <awsumatt@gmail.com>
2019-04-07 00:01:35 -0400

    Added board initialization

--- com/javafxChess/board/Board.java
+++ com/javafxChess/board/Board.java
@@ -6,7 +6,34 @@ package com.javafxChess.board;
 public static class Board{
 
 		public static Piece[][] makeBoard(){
-
+			return new Piece[8][8];
 		}
 
+		public static void initializeBoard(Piece[][] board){
+			//Black Team
+			board[0][0] = new Rook(false, new int[]{0, 0});
+			board[7][0] = new Rook(false, new int[]{7, 0});
+			board[1][0] = new Knight(false, new int[]{1, 0});
+			board[6][0] = new Knight(false, new int[]{6, 0});
+			board[2][0] = new Bishop(false, new int[]{2, 0});
+			board[5][0] = new Bishop(false, new int[]{5, 0});
+			board[3][0] = new Queen(false);
+			board[4][0] = new King(false);
+			for (int i=0; i<8; i++) { //Pawn loop
+				board[i][1] = new Pawn(false, new int[]{i, 1});
+			}
+
+			//White Team
+			board[0][7] = new Rook(true, new int[]{0, 7});
+			board[7][7] = new Rook(true, new int[]{7, 7});
+			board[1][7] = new Knight(true, new int[]{1, 7});
+			board[6][7] = new Knight(true, new int[]{6, 7});
+			board[2][7] = new Bishop(true, new int[]{2, 7});
+			board[5][7] = new Bishop(true, new int[]{5, 7});
+			board[3][7] = new Queen(true);
+			board[4][7] = new King(true);
+			for (int i=0; i<8; i++) { //Pawn loop
+				board[i][6] = new Pawn(true, new int[]{i, 6});
+			}
+		}
 }
