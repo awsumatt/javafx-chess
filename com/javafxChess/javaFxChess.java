5fb1e158d2bf050652b5e5a5046b434a2e3488ce
Matthew Jones <awsumatt@gmail.com>
2019-04-09 21:02:02 -0400

    Added imports for javafx

--- com/javafxChess/javaFxChess.java
+++ com/javafxChess/javaFxChess.java
@@ -1,6 +1,25 @@
 package com.javafxChess;
 
 
-public class javaFxChess{
-	
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.*;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+
+
+public class javaFxChess extends Application{
+
+@Override
+public void start(Stage primaryStage) {
+	Parent root = FXMLLoader.load(getClass().getResource("gameUI.fxml"));
+	Scene scene = new Scene(root);
+	primaryStage.setTitle("JavaFX Chess");
+	primaryStage.setScene(scene);
+	primaryStage.show();
+}
+
+
+
 }
