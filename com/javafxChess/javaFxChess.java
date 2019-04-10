b3cc568c0ffeda0a66a95715c244c42aa2c33d7a
Matthew Jones <awsumatt@gmail.com>
2019-04-10 13:17:48 -0400

    Trying to get UI to work

--- com/javafxChess/javaFxChess.java
+++ com/javafxChess/javaFxChess.java
@@ -2,17 +2,10 @@
 package com.javafxChess;
 
 
-<<<<<<< HEAD
 import javafx.fxml.FXML;
 import java.io.FileNotFoundException;
 import com.javafxChess.board.*;
 import com.javafxChess.pieces.*;
-=======
-import java.io.FileNotFoundException;
-import com.javafxChess.board.MoveLog;
-import com.javafxChess.board.Board;
-import com.javafxChess.pieces.Piece;
->>>>>>> acbed3e9e006a711513238e3a0745ebdc48452ef
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -24,29 +17,17 @@ import java.io.IOException;
 public class javaFxChess extends Application{
 
 	private boolean play = false;
-<<<<<<< HEAD
 	private boolean turn = true;
-=======
->>>>>>> acbed3e9e006a711513238e3a0745ebdc48452ef
 	private Piece[][] board;
 	private MoveLog log;
 
 
-
-<<<<<<< HEAD
 	@FXML
-=======
->>>>>>> acbed3e9e006a711513238e3a0745ebdc48452ef
 	public void startGame() throws FileNotFoundException{
 		play=true;
 		board = Board.makeBoard();
 		Board.initializeBoard(board);
 		log = new MoveLog();
-<<<<<<< HEAD
-
-
-=======
->>>>>>> acbed3e9e006a711513238e3a0745ebdc48452ef
 	}
 
 
