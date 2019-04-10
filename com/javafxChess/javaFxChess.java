acbed3e9e006a711513238e3a0745ebdc48452ef
Matthew Jones <awsumatt@gmail.com>
2019-04-10 12:35:10 -0400

    Working on UI

--- com/javafxChess/javaFxChess.java
+++ com/javafxChess/javaFxChess.java
@@ -1,10 +1,13 @@
 package com.javafxChess;
 
 
+import java.io.FileNotFoundException;
+import com.javafxChess.board.MoveLog;
+import com.javafxChess.board.Board;
+import com.javafxChess.pieces.Piece;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.event.*;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
 import java.io.IOException;
@@ -12,14 +15,28 @@ import java.io.IOException;
 
 public class javaFxChess extends Application{
 
-@Override
-public void start(Stage primaryStage) throws IOException {
-	Parent root = FXMLLoader.load(getClass().getResource("gameUI.fxml"));
-	Scene scene = new Scene(root);
-	primaryStage.setTitle("JavaFX Chess");
-	primaryStage.setScene(scene);
-	primaryStage.show();
-}
+	private boolean play = false;
+	private Piece[][] board;
+	private MoveLog log;
+
+
+
+	public void startGame() throws FileNotFoundException{
+		play=true;
+		board = Board.makeBoard();
+		Board.initializeBoard(board);
+		log = new MoveLog();
+	}
+
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("gameUI.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("JavaFX Chess");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
 
 
