6bc4e8f13152f2c3f57e891234f8a992117101f1
Matthew Jones <awsumatt@gmail.com>
2019-04-10 20:57:35 -0400

    initializing pieces with their image views

--- com/javafxChess/board/Board.java
+++ com/javafxChess/board/Board.java
@@ -1,5 +1,6 @@
 package com.javafxChess.board;
 
+import javafx.scene.image.ImageView;
 import com.javafxChess.pieces.*;
 
 
@@ -11,31 +12,33 @@ public class Board{
 			return new Piece[8][8];
 		}
 
-		public static void initializeBoard(Piece[][] board){
+		public static void initializeBoard(Piece[][] board, ImageView kingB, ImageView queenB, ImageView bishopB1, ImageView bishopB2, ImageView knightB1, ImageView knightB2, ImageView rookB1, ImageView rookB2, ImageView pawnB1, ImageView pawnB2, ImageView pawnB3, ImageView pawnB4, ImageView pawnB5, ImageView pawnB6, ImageView pawnB7, ImageView pawnB8, ImageView kingW, ImageView queenW, ImageView bishopW1, ImageView bishopW2, ImageView knightW1, ImageView knightW2, ImageView rookW1, ImageView rookW2, ImageView pawnW1, ImageView pawnW2, ImageView pawnW3, ImageView pawnW4, ImageView pawnW5, ImageView pawnW6, ImageView pawnW7, ImageView pawnW8){
 			//Black Team
-			board[0][0] = new Rook(false, new int[]{0, 0});
-			board[7][0] = new Rook(false, new int[]{7, 0});
-			board[1][0] = new Knight(false, new int[]{1, 0});
-			board[6][0] = new Knight(false, new int[]{6, 0});
-			board[2][0] = new Bishop(false, new int[]{2, 0});
-			board[5][0] = new Bishop(false, new int[]{5, 0});
-			board[3][0] = new Queen(false);
-			board[4][0] = new King(false);
+			ImageView[] bPawns = new ImageView[]{pawnB1, pawnB2, pawnB3, pawnB4, pawnB5, pawnB6, pawnB7, pawnB8};
+			board[0][0] = new Rook(false, new int[]{0, 0}, rookB1);
+			board[7][0] = new Rook(false, new int[]{7, 0}, rookB2);
+			board[1][0] = new Knight(false, new int[]{1, 0}, knightB1);
+			board[6][0] = new Knight(false, new int[]{6, 0}, knightB2);
+			board[2][0] = new Bishop(false, new int[]{2, 0}, bishopB1);
+			board[5][0] = new Bishop(false, new int[]{5, 0}, bishopB2);
+			board[3][0] = new Queen(false, queenB);
+			board[4][0] = new King(false, queenB);
 			for (int i=0; i<8; i++) { //Pawn loop
-				board[i][1] = new Pawn(false, new int[]{i, 1});
+				board[i][1] = new Pawn(false, new int[]{i, 1}, bPawns[i]);
 			}
 
 			//White Team
-			board[0][7] = new Rook(true, new int[]{0, 7});
-			board[7][7] = new Rook(true, new int[]{7, 7});
-			board[1][7] = new Knight(true, new int[]{1, 7});
-			board[6][7] = new Knight(true, new int[]{6, 7});
-			board[2][7] = new Bishop(true, new int[]{2, 7});
-			board[5][7] = new Bishop(true, new int[]{5, 7});
-			board[3][7] = new Queen(true);
-			board[4][7] = new King(true);
+			ImageView[] wPawns = new ImageView[]{pawnW1, pawnW2, pawnW3, pawnW4, pawnW5, pawnW6, pawnW7, pawnW8};
+			board[0][7] = new Rook(true, new int[]{0, 7}, rookW1);
+			board[7][7] = new Rook(true, new int[]{7, 7}, rookW2);
+			board[1][7] = new Knight(true, new int[]{1, 7}, knightW1);
+			board[6][7] = new Knight(true, new int[]{6, 7}, knightW2);
+			board[2][7] = new Bishop(true, new int[]{2, 7}, bishopW1);
+			board[5][7] = new Bishop(true, new int[]{5, 7}, bishopW2);
+			board[3][7] = new Queen(true, queenW);
+			board[4][7] = new King(true, kingW);
 			for (int i=0; i<8; i++) { //Pawn loop
-				board[i][6] = new Pawn(true, new int[]{i, 6});
+				board[i][6] = new Pawn(true, new int[]{i, 6}, wPawns[i]);
 			}
 		}
 }
