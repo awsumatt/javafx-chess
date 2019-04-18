809d44490c792bb290d21b98e87b783e8ede9ec0
Matthew Jones <awsumatt@gmail.com>
2019-04-18 17:40:02 -0400

    Removing castling and re-write of rook

--- com/javafxChess/pieces/Rook.java
+++ com/javafxChess/pieces/Rook.java
@@ -5,7 +5,6 @@ import javafx.scene.image.ImageView;
 import com.javafxChess.board.MoveLog;
 
 public class Rook extends Piece{
-	private Boolean canCastle;
 
 	/**
 	*Initializes rook with a specific location
@@ -16,48 +15,14 @@ public class Rook extends Piece{
 	public Rook(boolean team, int[] location, ImageView pic, GridPane pane) {
 		super(team, location, pic, pane);
 		this.setName("Rook");
-		canCastle = true;
 	}
 
-	/**
-	 * @param loc Location to move the piece
-	 * @param board The board the piece is on
-	 *
-	 * @return True if the move is valid, false otherwise
-	 */
-	@Override
-	public boolean move(int[] loc, Piece[][] board, MoveLog log) {
-		if(moveValid(loc, board)){
-			board[this.getX()][this.getY()] = null;
-			log.moveToString(this, location, loc);
-			location = loc;
-			ifEnemyRemove(loc, board, log);
-			board[this.getX()][this.getY()] = this;
-			canCastle = false;
-			GridPane.setColumnIndex(this.getPic(), loc[0]);
-			GridPane.setRowIndex(this.getPic(), loc[1]);
-			return true;
-		}
-		log.moveInv(this);
-		return false;
-	}
-
-	public Boolean getCanCastle() {
-		return canCastle;
-	}
-
-	public char getSide() {
-		if(location[0] < 5) {
-			return 'q';
-		}
-		return 'k';
-	}
 
 	public boolean moveValid(int[] loc, Piece[][] board) {
 		if(pathBlocked(loc, board)) {
-			if(getX() == loc[0] && getY() != loc[1] && moveValid(loc, board)) {;
+			if(super.getX()==loc[0]){ //Vertical
 				return true;
-			} else if(getX() != loc[0] && getY() == loc[1] && moveValid(loc, board)) {
+			} else if(super.getY()==loc[1]){ //Horizontal
 				return true;
 			}
 		}
